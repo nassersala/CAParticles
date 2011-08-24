939ccaa9d201704774cebe4e55db8f52fe0d9f60
Nasser <nassersala@gmail.com>
2011-08-24 20:19:53 +1000

    Initial Commit

---
+++ Particles/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Particles
+//
+//  Created by Nasser Ali on 8/24/11.
+//  Copyright (c) 2011 rmh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Particles/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Particles
+//
+//  Created by Nasser Ali on 8/24/11.
+//  Copyright (c) 2011 rmh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
